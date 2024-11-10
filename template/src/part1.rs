34b41bb6c1ea9f9d337a301d32afd3e7c288a846
Cory Johnson <cory1993johnson@gmail.com>
2024-11-10 09:37:39 +0100

    template setup

---
+++ template/src/part1.rs
@@ -0,0 +1,20 @@
+
+#[tracing::instrument]
+pub fn process(
+    _input: &str,
+) -> Result<String, String> {
+    todo!(" - part1");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> Result<(), String> {
+        todo!("haven't built test yet");
+        let input = "";
+        assert_eq!("", process(input)?);
+        Ok(())
+    }
+}
